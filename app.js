ba442a6469edc00f8bd72bae2ef6e75d2aa435fc
frankast <borodaev97@gmail.com>
2017-10-21 17:43:09 +0600

    Refactor

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res)=>{
+res.send('Cookies: ' + req.query.cookies);
+})
+app.listen(8091, ()=>{
+  console.log('App works on 8091 port...');
+});
